5ad180119fc6dd817abd96e3d24fac09e1573ed1
Evgeniy A. Dushistov <dushistov@mail.ru>
2016-10-26 02:48:09 +0300

    generate java code

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,14 @@ use syntex_pos::mk_sp;
 use syntex_syntax::util::small_vector::SmallVector;
 use syntex_syntax::print::pprust;
 use std::collections::HashMap;
+use std::env;
+use std::path::PathBuf;
+use std::fs::File;
+use std::error::Error;
+use std::io::prelude::*;
+//use std::fmt::Write;
 
+#[derive(PartialEq)]
 enum FuncVariant {
     Constructor, Method, StaticMethod
 }
@@ -33,6 +40,65 @@ impl FuncVariant {
             Some(FuncVariant::StaticMethod)
         } else {
             None
+        }
+    }
+}
+
+struct ForeignerMethod {
+    func_type: FuncVariant,
+    path: ast::Path,
+    in_out_type: P<ast::FnDecl>
+}
+
+struct TypesNames {
+    jni_type_name: &'static str,
+    java_type_name: &'static str
+}
+
+type RustToJavaTypes = HashMap<&'static str, TypesNames>;
+
+impl ForeignerMethod {
+    fn args(&self) -> String {
+        use std::fmt::Write;
+
+        let skip_n =  if self.func_type == FuncVariant::Method { 1 } else { 0 };
+        let mut res = String::new();
+        for (i, _) in self.in_out_type.inputs.iter().skip(skip_n).enumerate() {
+            if i == (self.in_out_type.inputs.len() - 1 - skip_n) {
+                write!(&mut res, "a_{}", i)
+            } else {
+                write!(&mut res, "a_{}, ", i)
+            }
+            .unwrap();
+        }
+        res
+    }
+
+    fn args_with_java_types(&self, types_map: &RustToJavaTypes) -> String {
+        use std::fmt::Write;
+
+        let skip_n =  if self.func_type == FuncVariant::Method { 1 } else { 0 };
+        let mut res = String::new();
+        for (i, item_arg) in self.in_out_type.inputs.iter().skip(skip_n).enumerate() {
+            let type_name = types_map.get(pprust::ty_to_string(&*item_arg.ty).as_str()).unwrap().java_type_name;
+            if i == (self.in_out_type.inputs.len() - 1 - skip_n) {
+                write!(&mut res, "{} a_{}", type_name, i)
+            } else {
+                write!(&mut res, "{} a_{}, ", type_name, i)
+            }
+            .unwrap();
+        }
+        res
+    }
+
+    fn java_return_type(&self) -> String {
+        "int".to_string()
+    }
+
+    fn short_name(&self) -> token::InternedString {
+        match self.path.segments.len() {
+            0 => token::InternedString::new(""),
+            n => self.path.segments[n - 1].identifier.name.as_str()
         }
     }
 }
@@ -181,6 +247,97 @@ fn add_args<'a, I>(builder: aster::expr::ExprCallArgsBuilder<aster::block::Block
     }
 }
 
+fn generate_java_code(rust_java_types_map: &RustToJavaTypes, package_name: &str, class_name: &token::InternedString, methods: &[ForeignerMethod], output_dir: &str) {
+    let mut path = PathBuf::from(output_dir);
+    path.push(format!("{}.java", class_name));
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+    write!(file,
+"package {};
+public final class {} {{
+    private long m_native;
+    public {}() {{
+        m_native = init();
+    }}
+    private static native long init();
+", package_name, class_name, class_name).unwrap();
+    for method_it in methods.iter() {
+        match method_it.func_type {
+            FuncVariant::Constructor | FuncVariant::StaticMethod => (),
+            FuncVariant::Method => {
+                write!(file,
+"
+    public {} {} ({}) {{ return do_{}(m_native, {}); }}
+    private static native {} do_{} (long me, {});
+",
+                       method_it.java_return_type(), method_it.short_name(), method_it.args_with_java_types(rust_java_types_map),
+                       method_it.short_name(), method_it.args(),
+                       method_it.java_return_type(), method_it.short_name(), method_it.args_with_java_types(rust_java_types_map)
+                ).unwrap();
+            }
+        }
+    }
+    write!(file, "}}").unwrap();
+}
+
+fn generate_rust_code(rust_java_types_map: &RustToJavaTypes, package_name: &str, class_name: &token::InternedString, methods: &[ForeignerMethod]) -> Box<MacResult> {
+    let package_name = package_name.replace(".", "_");
+    let builder = ::aster::AstBuilder::new();
+    let mut jni_methods = Vec::new();
+    for it in methods.iter() {
+        match it.func_type {
+            FuncVariant::Constructor | FuncVariant::StaticMethod => (),
+            FuncVariant::Method => {
+                let body_block = builder.block()
+                    .stmt().let_id("this").block().unsafe_().expr().call().id("jlong_to_pointer::<Foo>"/*TODO: calc Foo*/).arg().id("this").build()
+                    .stmt().let_id("this").block().unsafe_().expr().method_call("as_mut").id("this").build()
+                    .stmt().let_id("this").block().expr().method_call("unwrap").id("this").build()
+                    .expr().call().id(format!("{}", it.path)).arg().id("this");
+                let body_block = add_args(body_block, it.in_out_type.inputs.iter().skip(1), 0);
+                let func_name = it.short_name();
+                let fn_ = builder.item().fn_(format!("Java_{}_{}_{}", package_name,
+                                                     class_name,
+                                                     func_name
+                ))
+                    .arg_id("_").ty().id("*mut JNIEnv")
+                    .arg_id("_").ty().id("jclass")
+                    .arg_id("this").ty().id("jlong");
+                fn add_args_and_types<'a, I>(rust_java_types_map: &RustToJavaTypes, builder: aster::fn_decl::FnDeclBuilder<aster::item::ItemFnDeclBuilder<aster::invoke::Identity>>, mut iter: I, niter: usize) -> aster::fn_decl::FnDeclBuilder<aster::item::ItemFnDeclBuilder<aster::invoke::Identity>>
+                    where I: Iterator<Item=&'a Arg> {
+                    match iter.next() {
+                        Some(arg) => {
+                            let type_name = pprust::ty_to_string(&*arg.ty);
+                            let type_name = rust_java_types_map.get(type_name.as_str()).unwrap().jni_type_name;
+                            add_args_and_types(rust_java_types_map, builder.arg_id(format!("a_{}", niter)).ty().id(type_name), iter, niter + 1)
+                        }
+                        None => builder
+                    }
+                }
+
+                let fn_ = add_args_and_types(&rust_java_types_map, fn_, it.in_out_type.inputs.iter().skip(1), 0);
+                let fn_ = match &it.in_out_type.output {
+                    &ast::FunctionRetTy::Default(_) => fn_.return_().id("c_void"),
+                    &ast::FunctionRetTy::Ty(ref ret_type) => fn_.return_().id(
+                        rust_java_types_map.get(pprust::ty_to_string(&*ret_type).as_str()).unwrap().jni_type_name
+                    )
+                };
+                let fn_ = fn_.build(body_block.build());
+                let no_mangle_attr = builder.attr().word("no_mangle");
+                let fn_ = fn_.map(|mut p| {p.attrs.push(no_mangle_attr); p.vis = ast::Visibility::Public; p });
+                jni_methods.push(fn_);
+            }
+        }
+    }
+
+    MacEager::items(SmallVector::many(jni_methods))
+}
+
 fn expand_foreigner_class<'cx>(cx: &'cx mut ExtCtxt,
                                _: Span,
                                tokens: &[TokenTree])
@@ -195,7 +352,7 @@ fn expand_foreigner_class<'cx>(cx: &'cx mut ExtCtxt,
     let class_name_indent = parser.parse_ident().unwrap();
     println!("CLASS NAME {:?}", class_name_indent);
     parser.expect(&token::Token::OpenDelim(token::DelimToken::Brace)).unwrap();
-    let mut methods = Vec::<(FuncVariant, ast::Path, P<ast::FnDecl>)>::new();
+    let mut methods = Vec::new();
     loop {
         if parser.eat(&token::Token::CloseDelim(token::DelimToken::Brace)) {
             break;
@@ -219,62 +376,17 @@ fn expand_foreigner_class<'cx>(cx: &'cx mut ExtCtxt,
         println!("func_decl {:?}", func_decl);
         parser.expect(&token::Token::Semi).unwrap();
 
-        methods.push((func_type, func_name, func_decl));
+        methods.push(ForeignerMethod{func_type: func_type, path: func_name, in_out_type: func_decl});
     }
 
     let mut rust_java_types_map = HashMap::new();
-    rust_java_types_map.insert("i32", "jint");
+    rust_java_types_map.insert("i32", TypesNames{jni_type_name: "jint", java_type_name: "int"});
 
-    let package_name = "example_com";
-    let builder = ::aster::AstBuilder::new();
-    let mut jni_methods = Vec::new();
-    for it in methods.iter() {
-        match it.0 {
-            FuncVariant::Constructor | FuncVariant::StaticMethod => (),
-            FuncVariant::Method => {
-                let body_block = builder.block()
-                    .stmt().let_id("this").block().unsafe_().expr().call().id("jlong_to_pointer::<Foo>"/*calc Foo*/).arg().id("this").build()
-                    .stmt().let_id("this").block().unsafe_().expr().method_call("as_mut").id("this").build()
-                    .stmt().let_id("this").block().expr().method_call("unwrap").id("this").build()
-                    .expr().call().id(format!("{}", it.1)).arg().id("this");
-                let body_block = add_args(body_block, it.2.inputs.iter().skip(1), 0);
-                let func_name = match it.1.segments.len() {
-                    0 => token::InternedString::new(""),
-                    n => it.1.segments[n - 1].identifier.name.as_str(),
-                };
-                let fn_ = builder.item().fn_(format!("Java_{}_{}_{}", package_name,
-                                                     class_name_indent.name.as_str(),
-                                                     func_name
-                ))
-                    .arg_id("_").ty().id("*mut JNIEnv")
-                    .arg_id("_").ty().id("jclass")
-                    .arg_id("this").ty().id("jlong");
-                fn add_args_and_types<'a, I>(rust_java_types_map: &HashMap<&str, &str>, builder: aster::fn_decl::FnDeclBuilder<aster::item::ItemFnDeclBuilder<aster::invoke::Identity>>, mut iter: I, niter: usize) -> aster::fn_decl::FnDeclBuilder<aster::item::ItemFnDeclBuilder<aster::invoke::Identity>>
-                    where I: Iterator<Item=&'a Arg> {
-                    match iter.next() {
-                        Some(arg) => {
-                            let type_name = pprust::ty_to_string(&*arg.ty);
-                            let type_name = rust_java_types_map.get(type_name.as_str()).unwrap();
-                            add_args_and_types(rust_java_types_map, builder.arg_id(format!("a_{}", niter)).ty().id(type_name), iter, niter + 1)
-                        }
-                        None => builder
-                    }
-                }
-
-                let fn_ = add_args_and_types(&rust_java_types_map, fn_, it.2.inputs.iter().skip(1), 0);
-                let fn_ = match &it.2.output {
-                    &ast::FunctionRetTy::Default(_) => fn_.return_().id("c_void"),
-                    &ast::FunctionRetTy::Ty(ref ret_type) => fn_.return_().id(
-                        rust_java_types_map.get(pprust::ty_to_string(&*ret_type).as_str()).unwrap()
-                    )
-                };
-                let fn_ = fn_.build(body_block.build());
-                let no_mangle_attr = builder.attr().word("no_mangle");
-                let fn_ = fn_.map(|mut p| {p.attrs.push(no_mangle_attr); p.vis = ast::Visibility::Public; p });
-                jni_methods.push(fn_);
-            }
-        }
-    }
-
-    MacEager::items(SmallVector::many(jni_methods))
+    let java_output_dir = env::var("RUST_SWIG_JNI_JAVA_OUTPUT_DIR").unwrap();
+    let package_name = env::var("RUST_SWIG_JNI_JAVA_PACKAGE").unwrap();
+    
+    generate_java_code(&rust_java_types_map, package_name.as_str(), &class_name_indent.name.as_str(), &methods,
+                       &java_output_dir);
+    
+    generate_rust_code(&rust_java_types_map, package_name.as_str(), &class_name_indent.name.as_str(), &methods)
 }
