ec1bde970cf860c4e9387c5c242d7a6c65c5cb9f
Evgeniy A. Dushistov <dushistov@mail.ru>
2017-02-05 13:18:33 +0300

    run_tests.sh: enable assertions plus build if there is need

--- jni_tests/run_tests.sh
+++ jni_tests/run_tests.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ ! -f "src/jni.rs" ]]; then
+    ./generate_jni.sh
+fi
+
+cargo build
+
 cd src/com/example
 rm -f *.class
 rm -f *~
@@ -10,4 +16,4 @@ cd ../..
 rm -f *.jar
 jar cfv Test.jar com
 
-java -Djava.library.path=`pwd`/../target/debug -cp Test.jar com.example.Main
+java -ea -Djava.library.path=`pwd`/../target/debug -cp Test.jar com.example.Main
