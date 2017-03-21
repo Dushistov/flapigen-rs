8c60179edff17b8808f21cc01715d6b5f80e4c9f
Evgeniy A. Dushistov <dushistov@mail.ru>
2017-03-21 10:34:37 +0300

    some distribution of java have libtest.so, so rename our test library

--- jni_tests/src/com/example/Main.java
+++ jni_tests/src/com/example/Main.java
@@ -12,7 +12,7 @@ import com.example.TestInner;
 class Main {
     public static void main(String[] args) {
         try {
-            System.loadLibrary("test");
+            System.loadLibrary("rust_swig_test_jni");
         } catch (UnsatisfiedLinkError e) {
             System.out.println("Can not load library");
             throw e;
