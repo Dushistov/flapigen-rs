6afae167e6a61965ac9c260994e199b15a19fc2d
Evgeniy A. Dushistov <dushistov@mail.ru>
2018-03-13 06:28:09 +0300

    c++_tests: cmake cleanups

--- c++_tests/build.rs
+++ c++_tests/build.rs
@@ -25,16 +25,17 @@ fn main() {
     println!("cargo:rerun-if-changed=src");
     //rebuild if user remove generated code
     println!("cargo:rerun-if-changed={}", out_dir);
-    println!("cargo:rerun-if-changed=c++/rust_interface");
 }
 
 fn rust_swig_expand(from: &Path, out: &Path) -> Result<(), String> {
     println!("Run rust_swig_expand");
     let mut registry = syntex::Registry::new();
+    let cpp_gen_path = Path::new("c++").join("rust_interface");
+    println!("cargo:rerun-if-changed={}", cpp_gen_path.display());
     let cpp_cfg = if cfg!(feature = "boost") {
-        CppConfig::new(Path::new("c++").join("rust_interface"), "rust".into()).use_boost()
+        CppConfig::new(cpp_gen_path, "rust".into()).use_boost()
     } else {
-        CppConfig::new(Path::new("c++").join("rust_interface"), "rust".into())
+        CppConfig::new(cpp_gen_path, "rust".into())
     };
 
     let swig_gen = rust_swig::Generator::new(LanguageConfig::CppConfig(cpp_cfg));
