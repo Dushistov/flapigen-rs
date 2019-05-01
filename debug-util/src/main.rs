5df36e1946a3d683c896fb73c782b5a58991a55e
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-05-02 00:17:13 +0300

    infra: move command line utlity to new crate
    it is still impossible to use separate for bin,
    so move command line rust_swig from macroslib to external crate

---
+++ debug-util/src/main.rs
@@ -0,0 +1,45 @@
+use std::{env, fs, io::Read, path::Path};
+
+use rust_swig::{CppConfig, Generator, JavaConfig, LanguageConfig};
+
+fn main() {
+    env_logger::init();
+
+    let type_map = if let Some(type_map_pos) = env::args().position(|x| x == "--type-map") {
+        Some(
+            env::args()
+                .nth(type_map_pos + 1)
+                .expect("No path after --type-map"),
+        )
+    } else {
+        None
+    };
+    let use_cpp_lang = env::args().any(|x| x == "--cpp-lang");
+    let in_path = env::args().last().expect("No path to file to preprocess");
+    let in_path = Path::new(&in_path);
+
+    let cur_dir = env::current_dir().expect("Can not get current directory");
+    let out_path = cur_dir.join(if use_cpp_lang { "cpp" } else { "java" });
+    fs::create_dir_all(&out_path).unwrap_or_else(|why| {
+        panic!("! {:?}", why.kind());
+    });
+    let swig_gen = Generator::new(if !use_cpp_lang {
+        LanguageConfig::JavaConfig(JavaConfig::new(out_path.clone(), "com.example".into()))
+    } else {
+        LanguageConfig::CppConfig(CppConfig::new(out_path.clone(), "core".into()))
+    })
+    .with_pointer_target_width(64);
+
+    let swig_gen = if let Some(type_map) = type_map {
+        let mut tm_f = fs::File::open(&type_map)
+            .unwrap_or_else(|err| panic!("Can not open {:?}: {}", type_map, err));
+        let mut type_map = String::new();
+        tm_f.read_to_string(&mut type_map).unwrap();
+
+        swig_gen.merge_type_map("type_map", &type_map)
+    } else {
+        swig_gen
+    };
+
+    swig_gen.expand("test", in_path, out_path.join("out.rs"));
+}
