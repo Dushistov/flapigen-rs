b3d9759104823beadc1ecf4ae016d08fec124e0d
Adam Rzepka <adam@simteract.com>
2019-05-02 22:40:03 +0200

    Dummy static methods in python

--- macroslib/src/python/mod.rs
+++ macroslib/src/python/mod.rs
@@ -1,11 +1,17 @@
 use crate::{
-    error::Result, ForeignEnumInfo, ForeignInterface, ForeignerClassInfo, LanguageGenerator,
-    PythonConfig, TypeMap,
+    DiagnosticError, error::Result, ForeignEnumInfo, ForeignInterface, ForeignerClassInfo, ForeignerMethod, LanguageGenerator,
+    MethodVariant, PythonConfig, TypeMap,
 };
 use heck::SnakeCase;
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::Ident;
+
+fn method_name(method: &ForeignerMethod) -> Result<&syn::Ident> {
+    Ok(&method.rust_id.segments.last().ok_or_else(|| {
+        DiagnosticError::new(method.span(), "Method has no name")
+    })?.value().ident)
+}
 
 impl LanguageGenerator for PythonConfig {
     fn register_class(&self, conv_map: &mut TypeMap, class: &ForeignerClassInfo) -> Result<()> {
@@ -18,7 +24,44 @@ impl LanguageGenerator for PythonConfig {
         _: usize,
         class: &ForeignerClassInfo,
     ) -> Result<Vec<TokenStream>> {
-        Ok(vec![TokenStream::new()])
+        let class_name = &class.name;
+        let wrapper_mod_name = syn::parse_str::<Ident>(&format!(
+            "py_{}",
+            class_name.to_string().to_snake_case()
+        ))?;
+        self.module_initialization_code.borrow_mut().push(quote! {
+            {
+                m.add_class::<#wrapper_mod_name::#class_name>(py)?;
+            }
+        });
+        // let method_names = class.methods
+        //     .iter()
+        //     .filter(|m| m.variant == MethodVariant::Method)
+        //     .map(|m| m.short_name());
+        // let method_rust_paths = class.methods
+        //     .iter()
+        //     .filter(|m| m.variant == MethodVariant::Method)
+        //     .map(|m| m.rust_id);
+        let static_method_names = class.methods
+            .iter()
+            .filter(|m| m.variant == MethodVariant::StaticMethod)
+            .map(method_name)
+            .collect::<Result<Vec<_>>>()?;
+        let static_method_rust_paths = class.methods
+            .iter()
+            .filter(|m| m.variant == MethodVariant::StaticMethod)
+            .map(|m| &m.rust_id);
+        let class_code = quote! {
+            mod #wrapper_mod_name {
+                py_class!(pub class #class_name |py| {
+                    #( @staticmethod def #static_method_names() -> cpython::PyResult<cpython::PyObject> {
+                        super::#static_method_rust_paths();
+                        Ok(py.None()) 
+                    } )*
+                });
+            }
+        };
+        Ok(vec![class_code])
     }
 
     fn generate_enum(
@@ -62,7 +105,7 @@ impl LanguageGenerator for PythonConfig {
     fn init_glue_rs(&self, _conv_map: &mut TypeMap) -> Result<Vec<TokenStream>> {
         Ok(vec![quote! {
             #[macro_use] extern crate cpython;
-            use cpython::ObjectProtocol;
+            use cpython::{ObjectProtocol};
         }])
     }
 
--- python_tests/python/main.py
+++ python_tests/python/main.py
@@ -7,3 +7,4 @@ print("Doc: ", rust_swig_test_python.__doc__)
 print("Members: ", dir(rust_swig_test_python))
 
 print("TestEnum variants", dir(rust_swig_test_python.TestEnum))
+rust_swig_test_python.TestClass.print_hello()
