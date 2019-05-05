3960ed1c237684f7e7642c7f4cb1da5ff2ab212c
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-05-05 19:15:08 +0300

    infra: extend expectations/pass_objects_as_param
    comment #185

--- macroslib/tests/expectations/pass_objects_as_param.java
+++ macroslib/tests/expectations/pass_objects_as_param.java
@@ -1,3 +1,9 @@
 "public final void f1(long a0)";
-"public final void f2(@NonNull Foo a0)";
+r#"    public final void f2(@NonNull Foo a0)  {
+
+        long a0C0 = a0.mNativeObj;
+        a0.mNativeObj = 0;
+
+        do_f2(mNativeObj, a0C0);
+    }"#;
 "public final void f3(long a0)";
