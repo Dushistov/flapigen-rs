8cba6da85762defec7c52c46678e0180cfdc00b4
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-05-12 23:46:52 +0300

    c++/feature: make RustForeignVecIterator random_access_iterator

--- macroslib/src/cpp/rust_vec.h
+++ macroslib/src/cpp/rust_vec.h
@@ -97,6 +97,7 @@ struct CRustObjectSlice {
 
 #include <cassert>
 #include <type_traits>
+#include <iterator>
 
 namespace RUST_SWIG_USER_NAMESPACE {
 
@@ -222,7 +223,14 @@ using RustVecUsize = RustVec<CRustVecUsize, CRustVecUsize_free>;
 using RustVecF32 = RustVec<CRustVecF32, CRustVecF32_free>;
 using RustVecF64 = RustVec<CRustVecF64, CRustVecF64_free>;
 
-template <typename T> class RustForeignVecIterator final {
+template <typename T>
+class RustForeignVecIterator final
+    : public std::iterator<std::random_access_iterator_tag, // iterator_category
+                           T, // value_type
+                           ptrdiff_t, // difference_type
+                           const T *, // pointer
+                           T // reference
+                           > {
 public:
     using CForeignType = typename T::CForeignType;
 
@@ -236,23 +244,108 @@ public:
         , step(s)
     {
     }
+
+    /**
+     * \defgroup Forward iterator requirements
+     */
+    /*@{*/
+    T operator*() const noexcept
+    {
+        auto elem_ptr = static_cast<const CForeignType *>(this->ptr);
+        return T{ elem_ptr };
+    }
+
     RustForeignVecIterator &operator++() noexcept
     {
         this->ptr = static_cast<const uint8_t *>(this->ptr) + this->step;
         return *this;
     }
+    /*@}*/
 
-    bool operator==(const RustForeignVecIterator<T> &o) const noexcept
+    /**
+     * \defgroup Bidirectional iterator requirements
+     */
+    /*@{*/
+    RustForeignVecIterator &operator--() noexcept
+    {
+        this->ptr = static_cast<const uint8_t *>(this->ptr) - this->step;
+        return *this;
+    }
+    /*@}*/
+    /**
+     * \defgroup Random access iterator requirements
+     */
+    /*@{*/
+    T operator[](ptrdiff_t n) const noexcept
+    {
+        auto p = static_cast<const uint8_t *>(this->ptr) + n * this->step;
+        auto elem_ptr = static_cast<const CForeignType *>(p);
+        return T{ elem_ptr };
+    }
+
+    RustForeignVecIterator &operator+=(ptrdiff_t n) noexcept
+    {
+        this->ptr = static_cast<const uint8_t *>(this->ptr) + n * this->step;
+        return *this;
+    }
+
+    RustForeignVecIterator operator+(ptrdiff_t n) const noexcept
+    {
+        const void *p = static_cast<const uint8_t *>(this->ptr) + n * this->step;
+        return RustForeignVecIterator(p, this->step);
+    }
+
+    RustForeignVecIterator &operator-=(ptrdiff_t n) noexcept
+    {
+        this->ptr = static_cast<const uint8_t *>(this->ptr) - n * this->step;
+        return *this;
+    }
+
+    RustForeignVecIterator operator-(ptrdiff_t n) const noexcept
+    {
+        const void *p = static_cast<const uint8_t *>(this->ptr) - n * this->step;
+        return RustForeignVecIterator(p, this->step);
+    }
+
+    ptrdiff_t operator-(const RustForeignVecIterator &o) const noexcept
+    {
+        assert(this->step == o.step);
+        ptrdiff_t diff
+            = static_cast<const uint8_t *>(this->ptr) - static_cast<const uint8_t *>(o.ptr);
+        return diff / this->step;
+    }
+
+    constexpr bool operator<(const RustForeignVecIterator &o) const noexcept
+    {
+        assert(this->step == o.step);
+        return this->ptr < o.ptr;
+    }
+
+    constexpr bool operator>(const RustForeignVecIterator &o) const noexcept
+    {
+        assert(this->step == o.step);
+        return this->ptr > o.ptr;
+    }
+
+    constexpr bool operator<=(const RustForeignVecIterator &o) const noexcept
+    {
+        return !operator>(o);
+    }
+
+    constexpr bool operator>=(const RustForeignVecIterator &o) const noexcept
+    {
+        return !operator<(o);
+    }
+    /*@}*/
+
+    constexpr bool operator==(const RustForeignVecIterator<T> &o) const noexcept
     {
         assert(this->step == o.step);
         return this->ptr == o.ptr;
     }
-    bool operator!=(const RustForeignVecIterator<T> &o) const noexcept { return !operator==(o); }
-
-    T operator*() const noexcept
+    constexpr bool operator!=(const RustForeignVecIterator<T> &o) const noexcept
     {
-        auto elem_ptr = static_cast<const CForeignType *>(this->ptr);
-        return T{ elem_ptr };
+        return !operator==(o);
     }
 
 private:
