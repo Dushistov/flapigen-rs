d1da2f5ebf77d7af240940fd6adf06297de3d05c
Adam Rzepka <adam@simteract.com>
2019-05-20 16:53:26 +0200

    Python tests

--- python_tests/python/main.py
+++ python_tests/python/main.py
@@ -1,51 +1,71 @@
 #!/usr/bin/python3
 
-import rust_swig_test_python
+from rust_swig_test_python import TestStaticClass, TestEnum, TestClass, Error as TestError
 
-print("rust_swig_test_python module imported.")
-print("Doc: ", rust_swig_test_python.__doc__)
-print("Members: ", dir(rust_swig_test_python))
+def test_static_methods():
+    assert TestStaticClass.hello() == "Hello from rust"
+    assert TestStaticClass.format_number(123) == "format_number: 123"
+    assert TestStaticClass.format_str("python str") == "format_str: python str"
+    assert TestStaticClass.format_string("python string") == "format_string: python string"
+    assert TestStaticClass.add(1, 2) == 3
 
-print("TestEnum variants", dir(rust_swig_test_python.TestEnum))
-help(rust_swig_test_python.TestEnum)
-rust_swig_test_python.TestStaticClass.print_hello()
-rust_swig_test_python.TestStaticClass.print_number(123)
-rust_swig_test_python.TestStaticClass.print_str("python str")
-rust_swig_test_python.TestStaticClass.print_string("python string")
-print(rust_swig_test_python.TestStaticClass.add(1, 2))
-print(rust_swig_test_python.TestStaticClass.increment_vec([1, 2]))
-print(rust_swig_test_python.TestStaticClass.return_slice([3, 4]))
+def test_enum():
+    assert TestEnum.A == 0
+    assert TestEnum.B == 1
+    enum = TestEnum.A
+    assert TestStaticClass.reverse_enum(enum) == TestEnum.B
+    exception_occured = False
+    try:
+        # Pass invalid enum value
+        TestStaticClass.reverse_enum(2)
+    except ValueError as ex:
+        exception_occured = True
+    assert exception_occured
 
-test_class = rust_swig_test_python.TestClass()
-print(test_class)
+def test_class():
+    test_class = TestClass()
+    assert test_class.format() == "TestClass::i: 0"
+    test_class.increment()
+    assert test_class.format() == "TestClass::i: 1"
+    test_class.add(3)
+    assert test_class.get() == 4
+    # pass this class as an argument
+    assert TestStaticClass.call_test_class_format(test_class) == "TestClass::i: 4"
+    test_class.add_ref(1)
+    assert test_class.get_ref() == 5
 
-test_class.print()
-test_class.increment()
-test_class.print()
-test_class.add(3)
-test_class.print()
-print("test_class.get: ", test_class.get())
-print("test option (Some)", test_class.maybe_add(1))
-print("test option (None)", test_class.maybe_add(None))
 
-rust_swig_test_python.TestStaticClass.call_test_class_print(test_class)
+def test_options():
+    test_class = TestClass()
+    assert test_class.maybe_add(1) == 1
+    assert test_class.maybe_add(None) == None
 
-enum = rust_swig_test_python.TestEnum.A
-print("test_enum: ", enum)
-print("test_enum: ", rust_swig_test_python.TestStaticClass.reverse_enum(enum))
-try:
-    print("test_invalid: ", rust_swig_test_python.TestStaticClass.reverse_enum(2))
-except Exception as ex:
-    print(ex)
+def test_arrays():
+    assert TestStaticClass.increment_vec([1, 2]) == [2, 3]
+    assert TestStaticClass.return_slice([3, 4]) == [3, 4]
 
-# results:
-print(rust_swig_test_python.TestStaticClass.test_result_str_ok())
-try:
-    rust_swig_test_python.TestStaticClass.test_result_str_err()
-except rust_swig_test_python.Error as err:
-    print(err)
-print(rust_swig_test_python.TestStaticClass.test_result_ok())
-try:
-    rust_swig_test_python.TestStaticClass.test_result_err()
-except rust_swig_test_python.Error as err:
-    print(err)
+def test_results():
+    TestStaticClass.test_result_str_ok()
+    exception_occured = False
+    try:
+        TestStaticClass.test_result_str_err()
+    except TestError as ex:
+        exception_occured = True
+    assert exception_occured
+
+    TestStaticClass.test_result_ok()
+    exception_occured = False
+    try:
+        TestStaticClass.test_result_err()
+    except TestError as ex:
+        exception_occured = True
+    assert exception_occured
+
+print("Testing python API")
+test_enum()
+test_static_methods()
+test_class()
+test_options()
+test_arrays()
+test_results()
+print("Testing python API successful")
