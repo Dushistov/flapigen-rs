2cdc01696d9d5355c938174c7a1f1a2fcf71fe7a
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-06-11 12:03:25 +0300

    infra/tests: extend cpp_return_tuple

--- macroslib/tests/expectations/cpp_return_tuple.cpp
+++ macroslib/tests/expectations/cpp_return_tuple.cpp
@@ -1,3 +1,14 @@
 r#"public:
 
     std::pair<One, Two> f() const  noexcept"#;
+r#""#;
+
+r#"template<bool OWN_DATA>
+    inline std::pair<One, Two> FooWrapper<OWN_DATA>::f() const  noexcept
+    {
+        struct CRustObjectPair ret = Foo_f(this->self_);
+        return std::make_pair(One{static_cast<OneOpaque *>(ret.first)},
+ Two{static_cast<TwoOpaque *>(ret.second)});
+    }"#;
+
+"struct CRustObjectPair Foo_f(const FooOpaque * const self);";
