27896f490528c485a3b1bf83cecbfd598a9e1c92
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-06-29 01:41:08 +0300

    infra: extend tests for slice pass in/out

--- macroslib/tests/expectations/pass_slice_as_args.cpp
+++ macroslib/tests/expectations/pass_slice_as_args.cpp
@@ -1,3 +1,19 @@
 "CRustSliceU32 f1(RustForeignSlice<FooRef> a0) const";
+"struct CRustSliceU32 Boo_f1(const BooOpaque * const self, struct CRustObjectSlice a0);";
+r#"template<bool OWN_DATA>
+    inline  CRustSliceU32 BooWrapper<OWN_DATA>::f1(RustForeignSlice<FooRef> a0) const noexcept
+    {
 
+        struct CRustSliceU32 ret = Boo_f1(this->self_, a0);
+        return ret;
+    }"#;
+
+"struct CRustSliceU32 Boo_f2(const BooOpaque * const self, struct CRustObjectSlice a0);";
 "CRustSliceU32 f2(RustForeignSlice<FooRef> a0) const";
+r#"template<bool OWN_DATA>
+    inline  CRustSliceU32 BooWrapper<OWN_DATA>::f2(RustForeignSlice<FooRef> a0) const noexcept
+    {
+
+        struct CRustSliceU32 ret = Boo_f2(this->self_, a0);
+        return ret;
+    }"#;
--- macroslib/tests/expectations/return_slice.cpp
+++ macroslib/tests/expectations/return_slice.cpp
@@ -1,3 +1,30 @@
-"struct CRustObjectSlice Boo_f2(const BooOpaque *";
+"CRustSliceU32 f1() const noexcept;";
+"struct CRustSliceU32 Boo_f1(const BooOpaque * const self);";
+r#"template<bool OWN_DATA>
+    inline  CRustSliceU32 BooWrapper<OWN_DATA>::f1() const noexcept
+    {
 
-"RustForeignSlice<FooRef> f2() const";
+        struct CRustSliceU32 ret = Boo_f1(this->self_);
+        return ret;
+    }"#;
+
+"struct CRustObjectSlice Boo_f2(const BooOpaque * const self);";
+"RustForeignSlice<FooRef> f2() const noexcept;";
+r#"template<bool OWN_DATA>
+    inline RustForeignSlice<FooRef> BooWrapper<OWN_DATA>::f2() const noexcept
+    {
+
+        struct CRustObjectSlice ret = Boo_f2(this->self_);
+        return RustForeignSlice<FooRef>{ret};
+    }"#;
+
+"CRustSliceUsize f3() const noexcept;";
+"struct CRustSliceUsize Boo_f3(const BooOpaque * const self);";
+r#"template<bool OWN_DATA>
+    inline  CRustSliceUsize BooWrapper<OWN_DATA>::f3() const noexcept
+    {
+
+        struct CRustSliceUsize ret = Boo_f3(this->self_);
+        return ret;
+    }"#;
+
