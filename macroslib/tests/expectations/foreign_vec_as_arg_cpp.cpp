4acbaff74f0869568d95da16125b8f841219276f
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-06-29 03:28:51 +0300

    infra: extend c++ Vec<_> expectations tests

--- macroslib/tests/expectations/foreign_vec_as_arg_cpp.cpp
+++ macroslib/tests/expectations/foreign_vec_as_arg_cpp.cpp
@@ -1,3 +1,18 @@
-"RustForeignSlice<BooRef> alternateBoarding() const";
+"RustForeignSlice<BooRef> alternateBoarding() const noexcept;";
+"struct CRustObjectSlice FooImpl_alternateBoarding(const FooImplOpaque * const self);";
+r#"template<bool OWN_DATA>
+    inline RustForeignSlice<BooRef> FooImplWrapper<OWN_DATA>::alternateBoarding() const noexcept
+    {
 
-"void setAlternateBoarding(RustForeignVecBoo p)";
+        struct CRustObjectSlice ret = FooImpl_alternateBoarding(this->self_);
+        return RustForeignSlice<BooRef>{ret};
+    }"#;
+
+"void setAlternateBoarding(RustForeignVecBoo p) noexcept";
+"void FooImpl_setAlternateBoarding(FooImplOpaque * const self, struct CRustForeignVec p);";
+r#"template<bool OWN_DATA>
+    inline void FooImplWrapper<OWN_DATA>::setAlternateBoarding(RustForeignVecBoo p) noexcept
+    {
+
+        FooImpl_setAlternateBoarding(this->self_, p.release());
+    }"#;
