131baaf86d40832a038bf211fa6a596b0bcf073d
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-07-15 21:36:34 +0300

    infra: extend pass_objects_as_param (java)

--- macroslib/tests/expectations/pass_objects_as_param.java
+++ macroslib/tests/expectations/pass_objects_as_param.java
@@ -7,3 +7,13 @@ r#"    public final void f2(@NonNull Foo a0) {
         do_f2(mNativeObj, a00);
     }"#;
 "public final void f3(long a0)";
+r#"public final void f4(@NonNull Foo a0) {
+        long a00 = a0.mNativeObj;
+        do_f4(mNativeObj, a00);
+    }
+    private static native void do_f4(long self, long a0);"#;
+r#"public final void f5(@NonNull Foo a0) {
+        long a00 = a0.mNativeObj;
+        do_f5(mNativeObj, a00);
+    }
+    private static native void do_f5(long self, long a0);"#;
