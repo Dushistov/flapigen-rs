15579d32358ad6c394b34f93d542425aa045c563
Evgeniy A. Dushistov <dushistov@mail.ru>
2021-03-17 13:20:07 +0300

    c++/fix: as_ptr->as_mut_ptr in mut slice conversation

--- macroslib/tests/expectations/return_slice.rs
+++ macroslib/tests/expectations/return_slice.rs
@@ -1,14 +1,15 @@
 foreigner_class!(class Foo {
     self_type Foo;
     constructor Foo::new(_: i32) -> Foo;
-    method Foo::f(&self, _: i32, _: i32) -> i32;
+    fn Foo::f(&self, _: i32, _: i32) -> i32;
 });
 
 foreigner_class!(class Boo {
     self_type Boo;
 
     constructor Boo::new(_: i32, _: usize) -> Boo;
-    method Boo::f1(&self) -> &[u32];
-    method Boo::f2(&self) -> &[Foo];
-    method Boo::f3(&self) -> &[usize];
+    fn Boo::f1(&self) -> &[u32];
+    fn Boo::f2(&self) -> &[Foo];
+    fn Boo::f3(&self) -> &[usize];
+    fn Boo::f4(&self) -> &mut [usize];
 });
