734dfea8270dcdfd9be7dab70f2482b1915b8cfa
Evgeniy A. Dushistov <dushistov@mail.ru>
2023-04-16 18:42:01 +0300

    fix clippy warnings

--- macroslib/src/java_jni/java_code.rs
+++ macroslib/src/java_jni/java_code.rs
@@ -148,8 +148,8 @@ pub(in crate::java_jni) fn filter_null_annotation(type_name: &str) -> String {
 }
 
 pub(in crate::java_jni) fn is_primitive_type(type_name: &str) -> bool {
-    match type_name {
-        "void" | "boolean" | "byte" | "short" | "int" | "long" | "float" | "double" => true,
-        _ => false,
-    }
+    matches!(
+        type_name,
+        "void" | "boolean" | "byte" | "short" | "int" | "long" | "float" | "double"
+    )
 }
